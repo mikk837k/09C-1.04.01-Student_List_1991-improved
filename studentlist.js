3fef4e932411a786ffc3e4862e48226d851d09ec
Mikkel Madsen <mikkelmadsen@laptop-edlij0a3.kea.local>
2019-02-25 14:53:33 +0100

    initial commit

--- studentlist.js
+++ studentlist.js
@@ -2,36 +2,113 @@
 
 window.addEventListener("DOMContentLoaded", init);
 
+let studentArray;
+const newStudentArray = [];
+
+const student = {
+  fullname: "-fullname-",
+  firstname: "-firstname-",
+  lastname: "-lastname-",
+  imagename: "-imagename-",
+  house: "-house-",
+  blood_status: "-blood-",
+  expelled: "-expelled-",
+  inquisitorialSquad: "-inquisitorialSquad-",
+  setJSONData: function(studentData) {
+    this.fullname = studentData.fullname;
+    this.firstname = studentData.firstname;
+    this.lastname = studentData.lastname;
+    this.imagename = studentData.imagename;
+    this.house = studentData.house;
+    this.blood_status = studentData.blood_status;
+    this.expelled = studentData.expelled;
+    this.inquisitorialSquad = studentData.inquisitorialSquad;
+  }
+};
+
 function init() {
   console.log("init");
 
   // TODO: Load JSON, create clones, build list, add event listeners, show modal, find images, and other stuff ...
   getJSON();
+  let house = document.querySelectorAll(".filter_Button").forEach(button => {
+    button.addEventListener("click", getChosenHouse);
+  });
+  filterByHouse(house);
 }
 
-function getJSON() {
+async function getJSON() {
   console.log("getJSON");
+  let myJSON = await fetch("http://petlatkea.dk/2019/hogwarts/students.json");
+  studentArray = await myJSON.json();
 
-  // NOTE: Maybe also call sortByFirst the first time ... Investigate!
-  filterList();
+  generateNewArray();
 }
 
-function filterList() {
-  console.log("filterList");
+function generateNewArray() {
+  console.log("generateNewArray run");
+  studentArray.forEach(studentData => {
+    const newStudent = Object.create(student);
 
-  displayList();
+    newStudent.setJSONData(studentData);
+    newStudentArray.push(newStudent);
+  });
+  console.log(newStudentArray);
+
+  displayStudents(newStudentArray);
 }
 
-function displayList() {
-  console.log("displayList");
+function displayStudents(students) {
+  console.log("displayStudents");
+  let destination = document.querySelector("#data_student");
+  console.log(destination);
+  let myTemplate = document.querySelector("#data_template");
+
+  students.forEach(student => {
+    if (student.house == house || house == "all") {
+      console.log(myTemplate);
+      let clone = myTemplate.cloneNode(true).content;
+
+      // clone.querySelector("img").src = ;
+      clone.querySelector("h2").textContent = student.fullname;
+
+      destination.appendChild(clone);
+    }
+  });
 }
 
-// TODO: Create scaffolding functions for the rest!
+function getChosenHouse() {
+  console.log("filter has run");
 
-function clickSortByFirst() {
+  let house = "all";
 
+  house = this.getAttribute("data-house");
+
+  console.log(house);
+
+  return house;
+}
+function filterByHouse(chosenHouse) {
+  function filterHouse(student) {
+    return student.house === chosenHouse;
+  }
+  return newStudentArray.filter(filterHouse);
 }
 
-function sortListByFirst() {
+// function showStudentInfo() {}
 
-}
+// function filterList() {
+//   console.log("filterList");
+
+//   displayList();
+// }
+
+// function displayList() {
+//   console.log("displayList");
+// }
+
+// // TODO: Create scaffolding functions for the rest!
+
+// function clickSortByFirst() {}
+
+// function sortListByFirst() {}
